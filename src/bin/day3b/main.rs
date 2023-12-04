aa9e393ca1dcce9f25870adf7260a88eb370fb44
Jack Klamer <jack@jacks-mbp-2.lan>
2023-12-03 22:40:38 -0600

    day 3

--- src/bin/day3b/main.rs
+++ src/bin/day3b/main.rs
@@ -1,18 +1,17 @@
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::rc::Rc;
-
-use crate::PartOrSymbol::PART;
+use crate::PartOrGear::PART;
 
 fn main() {
-    let input = Path::new(file!()).parent().unwrap().join("input.txt").canonicalize().expect("Cannot locate file");
+    let input = Path::new(file!()).parent().unwrap().parent().unwrap().join("day3a/input.txt");
     let input = BufReader::new(File::open(input).expect("Input file missing"));
-    let mut index: HashMap<(i32, i32), PartOrSymbol> = HashMap::new();
-    let mut nums: Vec<Rc<PartNum>> = Vec::new();
 
+    let mut index: HashMap<(i32, i32), PartOrGear> = HashMap::new();
+    let mut gears: Vec<Rc<RefCell<HashSet<Rc<PartNum>>>>> = Vec::new();
     for (i, line) in input.lines().enumerate() {
         let i = i as i32;
         let line = line.expect("IO error");
@@ -23,47 +22,57 @@ fn main() {
                 b'0'..=b'9' => {
                     let (num, num_end) = parse_part_num(j as usize, line);
                     let num = Rc::new(PartNum::new(num));
-                    nums.push(num.clone());
                     for j_1 in j..(num_end as i32) {
                         index.insert((i, j_1), PART(num.clone()));
                     }
                     for j_2 in (j - 1)..=(num_end as i32) {
-                        if let Some(p) = index.get(&((i - 1), j_2))
+                        if let Some(pog) = index.get(&((i - 1), j_2))
                         {
-                            if let PartOrSymbol::SYMBOL = p {
-                                num.mark_valid()
+                            if let PartOrGear::GEAR(parts) = pog {
+                                parts.borrow_mut().insert(num.clone());
                             }
                         }
                     }
-                    if let Some(p_or_s) = index.get(&(i, (j - 1)))
+                    if let Some(pog) = index.get(&(i, (j - 1)))
                     {
-                        if let PartOrSymbol::SYMBOL = p_or_s {
-                            num.mark_valid();
+                        if let PartOrGear::GEAR(parts) = pog {
+                            parts.borrow_mut().insert(num.clone());
                         }
                     }
                     j = num_end as i32 - 1;
                 }
-                b'.' => {}
-                _ => {
-                    index.insert((i, j), PartOrSymbol::SYMBOL);
+                b'*' => {
+                    let parts = Rc::new(RefCell::new(HashSet::new()));
+                    gears.push(parts.clone());
+                    index.insert((i, j), PartOrGear::GEAR(parts.clone()));
                     for j_1 in (j - 1)..=(j + 1) {
-                        if let Some(p_or_s) = index.get(&((i - 1), j_1))
+                        if let Some(pog) = index.get(&((i - 1), j_1))
                         {
-                            p_or_s.mark_valid();
+                            if let PART(part) = pog {
+                                parts.borrow_mut().insert(part.clone());
+                            }
                         }
                     }
-                    if let Some(p_or_s) = index.get(&(i, (j - 1)))
+                    if let Some(pog) = index.get(&(i, (j - 1)))
                     {
-                        p_or_s.mark_valid();
+                        if let PART(part) = pog {
+                            parts.borrow_mut().insert(part.clone());
+                        }
                     }
                 }
+                _ => {}
             };
             j += 1;
         }
     }
 
-    let sum: u64 = nums.into_iter().map(|p| p.get_valid_num()).flatten().sum();
-    println!("Part num sum {}", sum);
+   let sum: u64 = gears.iter()
+       .filter(|parts| parts.borrow().len() == 2)
+       .map(|parts| parts.borrow().iter()
+           .map(|p| p.num)
+           .product::<u64>())
+       .sum();
+    println!("Gear sum {}", sum);
 }
 
 fn parse_part_num(mut idx: usize, line: &[u8]) -> (u64, usize) {
@@ -77,43 +86,20 @@ fn parse_part_num(mut idx: usize, line: &[u8]) -> (u64, usize) {
 }
 
 #[derive(Debug)]
-enum PartOrSymbol {
+enum PartOrGear {
     PART(Rc<PartNum>),
-    SYMBOL,
+    GEAR(Rc<RefCell<HashSet<Rc<PartNum>>>>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Hash, PartialEq, Eq)]
 struct PartNum {
-    is_valid: RefCell<bool>,
     num: u64,
-}
-
-impl PartOrSymbol {
-    fn mark_valid(&self) {
-        if let PART(num) = self {
-            num.mark_valid();
-        }
-    }
 }
 
 impl PartNum {
     pub(crate) fn new(num: u64) -> PartNum {
         PartNum {
-            is_valid: RefCell::new(false),
             num,
         }
     }
-
-    pub(crate) fn mark_valid(&self) {
-        self.is_valid.replace(true);
-    }
-
-    fn get_valid_num(&self) -> Option<u64> {
-        if *self.is_valid.borrow() {
-            Some(self.num)
-        } else {
-            None
-        }
-    }
 }
-
